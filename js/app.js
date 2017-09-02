de08ca34d1b9191900760647d321dc365a92e065
Samuel Parra <samuelparra1@gmail.com>
2017-09-02 12:52:22 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// const createVideoList = (video, parent) => {
+const createVideoList = () => {
+  let template = document.getElementById('video_thumbnail');
+
+}
---
+++ js/signup.js
@@ -0,0 +1,10 @@
+// const valid = false;
+// const check = (form) =>{
+//   if(form.password.value == form.passwordConfirmation.value){
+//     document.getElementById('pc').className = "col-11 has-success";
+//     valid = true;
+//   }else{
+//     document.getElementById('pc').className = "col-11 has-danger";
+//     valid = true;
+//   }
+// }
